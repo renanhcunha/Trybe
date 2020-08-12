28f8b9689e883eb51f81bd97e8c611b7f729b3ef
Renan Cunha <renan2610@gmail.com>
2020-08-12 15:43:26 -0300

    removing console.logs

--- Modulo 2 - Front-End/Bloco 7 - Javascript ES6 e Testes unitários/exercises/7_2/exercise03.js
+++ Modulo 2 - Front-End/Bloco 7 - Javascript ES6 e Testes unitários/exercises/7_2/exercise03.js
@@ -23,28 +23,23 @@ const lesson3 = {
 const newProperty = (object, key, value) => object[key] = value;
 
 newProperty(lesson2, 'turno', 'manhã');
-// console.log(lesson2);
 
 // 2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
 const listKeys = object => Object.keys(object);
 
-// console.log(`As chaves do objeto são: ${listKeys(lesson3)}`)
-
 // 3. Crie uma função para mostrar o tamanho de um objeto.
 
 const objectLength = object => Object.entries(object).length;
-// console.log(objectLength(lesson1));
 
 // 4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
 const objectValues = object => Object.values(object);
-// console.log(objectValues(lesson3));
 
 // 5. Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. 
 
-const allLessons = {lesson1, lesson2, lesson3};
-console.log(allLessons);
+const allLessons = {};
+Object.assign(allLessons, {lesson1, lesson2, lesson3})
 
 // 6. Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 
@@ -56,12 +51,10 @@ const totalStudents = (object) => {
   }
   return total;
 }
-// console.log(totalStudents(allLessons));
 
 // 7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
 
 const keyValue = (object, key) => Object.values(object)[key];
-// console.log(keyValue(lesson1, 0));
 
 // 8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
 
@@ -73,5 +66,3 @@ let letFindKeyValue = (object, key, value) => {
   }
   return false;
 }
-
-console.log(letFindKeyValue(lesson3, 'professor', 'Maria Clara'));
