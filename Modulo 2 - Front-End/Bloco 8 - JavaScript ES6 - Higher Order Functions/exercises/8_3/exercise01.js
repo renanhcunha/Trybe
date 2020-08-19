f861b08dc52a65493e9fddc92cca0d201d5dc394
Renan Cunha <renan2610@gmail.com>
2020-08-19 20:27:03 -0300

    exercises - block 8.3

---
+++ Modulo 2 - Front-End/Bloco 8 - JavaScript ES6 - Higher Order Functions/exercises/8_3/exercise01.js
@@ -0,0 +1,16 @@
+    
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+// Dada uma matriz de matrizes, transforme em uma única matriz.
+
+function flatten() {
+  return arrays.reduce((acc, element) => acc.concat(element));
+}
+
+assert.deepEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
