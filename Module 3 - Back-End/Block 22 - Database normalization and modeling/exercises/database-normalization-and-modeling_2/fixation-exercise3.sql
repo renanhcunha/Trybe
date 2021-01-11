0df33bc6a455981aabf0c7165b4d97018880fdc8
Renan Cunha <renan2610@gmail.com>
2021-01-11 18:14:12 -0300

    updating fixation-exercise3

--- Module 3 - Back-End/Block 22 - Database normalization and modeling/exercises/database-normalization-and-modeling_2/fixation-exercise3.sql
+++ Module 3 - Back-End/Block 22 - Database normalization and modeling/exercises/database-normalization-and-modeling_2/fixation-exercise3.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS normalization;
 
-use normalization;
+USE normalization;
 
 CREATE TABLE funcionario (
 Funcionario_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -10,6 +10,8 @@ Email VARCHAR(40) NOT NULL,
 Telefone VARCHAR(40),
 DataCadastro DATETIME NOT NULL
 );
+
+ALTER TABLE funcionario AUTO_INCREMENT = 12;
 
 CREATE TABLE setores (
 Setor_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,8 +25,26 @@ FOREIGN KEY (Funcionario_id) REFERENCES funcionario(Funcionario_id),
 FOREIGN KEY (Setor_id) REFERENCES setores(Setor_id)
 );
 
--- INSERT INTO table_name (column1, column2, column3, ...)
--- VALUES (value1, value2, value3, ...);
+INSERT INTO funcionario (Nome, Sobrenome, Email, Telefone, DataCadastro)
+VALUES
+('Joseph', 'Rodrigues', 'jo@gmail.com', '(35)998552-1445', '2020-05-05 08:50:25'),
+('André', 'Freeman', 'andre1990@gmail.com', '(47)99522-4996', '2020-02-05 00:00:00'),
+('Cíntia', 'Duval', 'cindy@outlook.com', '(33)99855-4669', '2020-05-05 10:55:35'),
+('Fernanda', 'Mendes', 'fernandamendes@yahoo.com', '(33)99200-1556', '2020-05-05 11:45:40');
 
--- INSERT INTO table_name -- Todos os valores
--- VALUES (value1, value2, value3, ...);
+INSERT INTO setores (Setor)
+VALUES
+('Administração'),
+('Vendas'),
+('Operacional'),
+('Estratégico'),
+('Marketing');
+
+INSERT INTO setor_funcionario (Funcionario_id, Setor_id)
+VALUES
+(12, 1),
+(12, 2),
+(13, 3),
+(14, 4),
+(14, 2),
+(15, 5);
