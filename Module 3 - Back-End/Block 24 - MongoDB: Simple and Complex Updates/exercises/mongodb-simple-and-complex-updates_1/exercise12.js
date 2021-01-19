025e51f636d9f85eccd2142960e49260a36e0bf8
Renan Cunha <renan2610@gmail.com>
2021-01-19 19:11:27 -0300

    Adding exercises 1 to 13

---
+++ Module 3 - Back-End/Block 24 - MongoDB: Simple and Complex Updates/exercises/mongodb-simple-and-complex-updates_1/exercise12.js
@@ -0,0 +1,3 @@
+// Exercício 12 : Utilizando uma única operação, remova os campos budget e estimatedBudget em todos os documentos.
+
+db.movies.updateMany({}, {$unset: {"budget": "", "estimatedBudget": ""}})
